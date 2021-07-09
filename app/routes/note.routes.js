68d7c5f44e6a4fd8150fc7125bac44714e32b94f
Dogusyak <doguhanyangoz@gmail.com>
2021-07-09 12:10:08 +0200

    added functionality

--- app/routes/note.routes.js
+++ app/routes/note.routes.js
@@ -1,20 +1,21 @@
 module.exports = (app) => {
-    const notes = require('../controllers/note.controllers.js');
+  const notes = require("../controllers/note.controllers.js");
 
-    // Create a new Note
-    app.post('/notes', notes.create);
+  // Create a new Note
+  app.post("/notes", notes.create);
 
-    // Retrieve all Notes
-    app.get('/notes', notes.findAll);
+  // Retrieve all Notes
+  app.get("/notes", notes.findAll);
 
-    // Retrieve a single Note with noteId
-    app.get('/notes/:noteId', notes.findOne);
-    
-    app.get('/notes/sortBy/:sortId', notes.findAll);
+  // Retrieve a single Note with noteId
+  app.get("/notes/:noteId", notes.findOne);
 
-    // Update a Note with noteId
-    app.put('/notes/:noteId', notes.update);
+  app.get("/notes/sortBy/:sortId", notes.findAll);
 
-    // Delete a Note with noteId
-    app.delete('/notes/:noteId', notes.delete);
-}
+  // Update a Note with noteId
+  app.put("/notes/:noteId", notes.update);
+  app.put("/notes/checked/:noteId", notes.updateChecked);
+
+  // Delete a Note with noteId
+  app.delete("/notes/:noteId", notes.delete);
+};
--- views/scripts/script.js
+++ views/scripts/script.js
@@ -1,83 +1,121 @@
-document.getElementById("encode").addEventListener('click', postFunction);
+document.getElementById("encode").addEventListener("click", postFunction);
 
 function postFunction() {
-    var title_temp = document.getElementById("title").value;
-    var content_temp = document.getElementById("content").value;
-    var importance_temp = document.getElementById("importance").value;
-    var due_temp = document.getElementById("due").value;
-    let response = fetch('http://localhost:3000/notes', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({title: title_temp, content: content_temp, importance: importance_temp, due: due_temp})
+  var title_temp = document.getElementById("title").value;
+  var content_temp = document.getElementById("content").value;
+  var importance_temp = document.getElementById("importance").value;
+  var due_temp = document.getElementById("due").value;
+  let response = fetch("http://localhost:3000/notes", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title: title_temp,
+      content: content_temp,
+      importance: importance_temp,
+      due: due_temp,
+      checked: true,
+    }),
+  }).then((response) => {
+    console.log(response);
+  });
+}
+function updateFunction() {
+  var title_temp = document.getElementById("titleMod").value;
+  var content_temp = document.getElementById("contentMod").value;
+  var importance_temp = document.getElementById("importanceMod").value;
+  var due_temp = document.getElementById("dueMod").value;
+  var id_temp = document.getElementById("idMod").value;
+  let response = fetch("http://localhost:3000/notes/" + id_temp, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title: title_temp,
+      content: content_temp,
+      importance: importance_temp,
+      due: due_temp,
+    }),
+  }).then((response) => {
+    modal.style.display = "none";
+    getFunction();
+  });
+}
 
-    }).then(response => {
-        console.log(response)
+document.getElementById("tabtwo").addEventListener("click", getFunction);
+function getFunction(sortBy = "_id") {
+  var mainContainer = document.getElementById("tabtwo2");
+  var checked = document.getElementById("done");
+  function isChecked(data) {
+    return data._id != true;
+  }
+  let request = fetch("http://localhost:3000/notes/sortBy/" + sortBy)
+    .then(function (response) {
+      return response.json();
     })
+    .then(function (data) {
+      document.getElementById("notes").innerHTML = "";
+      for (let i = 0; i < data.length; i++) {
+        console.log(checked.checked);
+        if (checked.checked === false && data[i].checked === true) {
+          continue;
+        }
+        var div = document.createElement("div");
+        div.setAttribute("id", "note");
+        div.innerHTML +=
+          "<p>Due:" +
+          data[i].due +
+          "</p><p class='note-title'>" +
+          data[i].title +
+          "</p><p></p><div><label for='finished'>Finished</label><input type='checkbox' id='check' name='checkbox'" +
+          (data[i].checked === true ? "checked" : "") +
+          "></div>" +
+          "<p class='note-content'>" +
+          data[i].content +
+          "</p><button id='edit' onclick='openModal(\"" +
+          data[i]._id +
+          "\")'>Edit</button><br><br>";
+        document.getElementById("notes").appendChild(div);
+      }
+    });
 }
-
-
-
-
-
-document.getElementById("tabtwo").addEventListener('click', getFunction);
-function getFunction(sortBy="") {
-    var mainContainer = document.getElementById("tabtwo2");
-    // all buttons for sort
-    
-    let request = fetch('http://localhost:3000/notes/sortBy/'+sortBy)
-        .then(function (response) {
-            return response.json();
-        }).then(function (data) {
-            document.getElementById("notes").innerHTML="";
-            for(let i = 0; i < data.length; i++){
-                var div = document.createElement("div");
-                div.setAttribute("id", "note")
-                div.innerHTML += "<p>Due:" + data[i].due + "</p><p class='note-title'>"+data[i].title + "</p><p></p><div><label for='finished'>Finished</label><input type='checkbox' id='check' name='checkbox'></div>" + "<p class='note-content'>" +data[i].content + "</p><button id='edit' onclick='openModal(\"" + data[i]._id + "\")'>Edit</button><br><br>";
-                document.getElementById("notes").appendChild(div);
-            }
-
-            
-        });
-}
-
 
 // Modal not in use at the moment
 
-function openModal(id){
-    const openMod = document.getElementById("edit");
-    const modal = document.getElementById("modal");
-    const closeMod = document.getElementById("exit");
-    var content_temp = document.getElementById("contentMod");
+function openModal(id) {
+  const openMod = document.getElementById("edit");
+  const modal = document.getElementById("modal");
+  const closeMod = document.getElementById("exit");
+  const saveMod = document.getElementById("save");
+  var content_temp = document.getElementById("contentMod");
 
+  modal.style.display = "block";
 
-    modal.style.display = "block";
+  closeMod.onclick = function () {
+    modal.style.display = "none";
+  };
+  saveMod.onclick = function () {
+    updateFunction();
+  };
 
-    closeMod.onclick = function(){
-        modal.style.display = "none";
-    }
-
-    let response = fetch('http://localhost:3000/notes/' + id).then(response => {
-        return response.json()
-    }).then(function(data){
-        document.getElementById("titleMod").value = data.title;
-        document.getElementById("contentMod").value = data.content;
-        document.getElementById("importanceMod").value = data.importance;
-        document.getElementById("dueMod").value = data.due;
+  let response = fetch("http://localhost:3000/notes/" + id)
+    .then((response) => {
+      return response.json();
     })
+    .then(function (data) {
+      document.getElementById("idMod").value = data._id;
+      document.getElementById("titleMod").value = data.title;
+      document.getElementById("contentMod").value = data.content;
+      document.getElementById("importanceMod").value = data.importance;
+      document.getElementById("dueMod").value = data.due;
+    });
 }
 
-
-
-    
-
-    // closeMod.addEventListener('click', () => {
-    //     modal.classList.remove('display');
-    // });
-
-
-
+// closeMod.addEventListener('click', () => {
+//     modal.classList.remove('display');
+// });
 
 // document.getElementById("check").addEventListener('click', check);
 // function check(){
@@ -89,5 +127,3 @@ function openModal(id){
 //         alert("no check");
 //     }
 // }
-
-
